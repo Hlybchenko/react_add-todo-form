53af4c946488d1962628e70ca2703a9cad09e0fa
OleksandrHlybchenko <logista@meta.ua>
2019-07-18 02:56:42 +0300

    Updates

---
+++ precache-manifest.926f342c3435bc495566235b74fd7e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb4e7891d16ecf3b32f70941b8138ee",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "a87d4d8e345f9647061d",
+    "url": "/react_add-todo-form/static/css/main.3e2db1f5.chunk.css"
+  },
+  {
+    "revision": "f61578276625cefcacf3",
+    "url": "/react_add-todo-form/static/js/2.db99b796.chunk.js"
+  },
+  {
+    "revision": "a87d4d8e345f9647061d",
+    "url": "/react_add-todo-form/static/js/main.60782cee.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
